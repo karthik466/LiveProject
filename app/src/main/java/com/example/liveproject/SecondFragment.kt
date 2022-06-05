c143049cb89ca1e568d222d0015db26e074ce820
KARTHIK <dev4karthik@gmail.com>
2022-06-05 11:14:34 +0530

    * initial demo completed

--- app/src/main/java/com/example/liveproject/SecondFragment.kt
+++ app/src/main/java/com/example/liveproject/SecondFragment.kt
@@ -1,12 +1,10 @@
 package com.example.liveproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.liveproject.databinding.FragmentSecondBinding
 
 /**
