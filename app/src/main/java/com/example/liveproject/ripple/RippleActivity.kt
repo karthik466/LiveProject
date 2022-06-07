d0f17234c05ca498aa670393559c1dd27d46e1d5
KARTHIK <dev4karthik@gmail.com>
2022-06-07 23:23:32 +0530

    * initial RippleActivity.kt

---
+++ app/src/main/java/com/example/liveproject/ripple/RippleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.liveproject.ripple
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.liveproject.R
+
+class RippleActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ripple_animation)
+    }
+}
