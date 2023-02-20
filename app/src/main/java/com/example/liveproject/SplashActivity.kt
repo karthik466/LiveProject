ef0ecca9c43461996470f538f467c76f12073c43
KARTHIK <dev4karthik@gmail.com>
2023-02-21 00:03:23 +0530

    * Added SplashActivity.kt

---
+++ app/src/main/java/com/example/liveproject/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.liveproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_splash)
+    }
+}
